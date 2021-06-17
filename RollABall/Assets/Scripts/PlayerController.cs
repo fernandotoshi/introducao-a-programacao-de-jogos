65fdc66c65621948da46e271dffcccc783d49062
fernandotoshi <fernando.s.akamine@gmail.com>
2021-06-16 23:02:02 -0300

    Primeiro jogo, não deveria ter adicionado tudo

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public int score;
+    public Text scoreText;
+    public Text gameOverText;
+    public Text countdownText;
+
+    public float Countdown;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        gameOverText.gameObject.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Countdown = Mathf.Clamp(Countdown - Time.deltaTime, 0, 100);
+        countdownText.text = "Countdown: " + Mathf.CeilToInt(Countdown);
+
+        if (Countdown <= 0 && !gameOverText.gameObject.activeSelf)
+        {
+            gameOverText.text = "Game Over!";
+            gameOverText.gameObject.SetActive(true);
+        }
+
+        if (score == 16 && !gameOverText.gameObject.activeSelf)
+        {
+            gameOverText.text = "You win!";
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(x, 0, y);
+        force.Normalize();
+        force *= speed;
+
+        rb.AddForce(force);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            //other.gameObject.SetActive(false); // Deactivate gameObject
+            Destroy(other.gameObject); // Destroy gameObject
+
+            score++;
+            scoreText.text = "Points: " + score;
+        }
+    }
+}
---
+++ RollABall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+
+        speed = Mathf.Clamp(speed - Time.deltaTime, 1, 10);
+    }
+}
